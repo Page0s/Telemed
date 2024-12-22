2d777b098ea92a7114d00c4c3e3cc12f61922eb6
Bojan Pađen <game4rge@gmail.com>
2024-12-22 20:00:35 +0100

    add PatientEntry.java

---
+++ src/main/java/com/telemed/telemed/model/PatientEntry.java
@@ -0,0 +1,68 @@
+package com.telemed.telemed.model;
+
+public class PatientEntry {
+    private int id;
+    private int heartRate;
+    private int systolic;
+    private int diastolic;
+    private String date;
+    private String description;
+
+    public PatientEntry(int id, int heartRate, int systolic, int diastolic, String date, String description) {
+        this.id = id;
+        this.heartRate = heartRate;
+        this.systolic = systolic;
+        this.diastolic = diastolic;
+        this.date = date;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public int getSystolic() {
+        return systolic;
+    }
+
+    public void setSystolic(int systolic) {
+        this.systolic = systolic;
+    }
+
+    public int getDiastolic() {
+        return diastolic;
+    }
+
+    public void setDiastolic(int diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
